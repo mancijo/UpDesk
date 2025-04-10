d1d55daa49e053717cf77eccafffe921d49654cc
mateus <mateus.-teodoro22@outlook.com>
2025-04-09 23:24:20 -0300

    Codigo em C#
    Construcao do CRUD em C#

---
+++ UpDesk_C#/SuporteN1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpDesk
+{
+    class SuporteN1 : Usuario
+    {
+        public SuporteN1(string login, string senha) : base(login, senha)
+        {
+        }
+    }
+}
