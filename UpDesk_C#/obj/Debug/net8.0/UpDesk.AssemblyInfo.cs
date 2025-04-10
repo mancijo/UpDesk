b449474a55ae54c7ac6e225f4d5ff93e0a1d8c6b
Mateus Teodoro da Silva <161161001+MateusTeod@users.noreply.github.com>
2025-04-10 05:50:33 -0300

    Corrigindo bugs e erros de sintaxe
    Foram corrigidos erros de sintaxe no codigo, tambem corrigido alguns, mudanca das classe Usuario e chamados para private fazendo o encapsulamento do codigo

--- UpDesk_C#/obj/Debug/net8.0/UpDesk.AssemblyInfo.cs
+++ UpDesk_C#/obj/Debug/net8.0/UpDesk.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UpDesk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee8659610fc0fb2f397fdf44984fa80b33fed67f")]
 [assembly: System.Reflection.AssemblyProductAttribute("UpDesk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UpDesk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
