0d53b8399904a19939fd695b299cca982e2fa1f2
Jonatas Santos <jonatassouza228@outlook.com>
2025-08-31 15:17:24 -0300

    Tabela criar usuário
    Criação de exemplo de criação de usuário

---
+++ BD/Criando usuário.sql
@@ -0,0 +1,19 @@
+INSERT INTO Usuario (nome, email, senha, setor, user_type)
+VALUES ('Kaique Silva', 'kaique.silva@updesk.com', '123456', 'TI', 'Supervisor');
+
+INSERT INTO Usuario (nome, email, senha, setor, user_type)
+VALUES ('Matheus Teodoro', 'matheus.teodoro@updesk.com', '123456', 'TI', 'Supervisor');
+
+INSERT INTO Usuario (nome, email, senha, setor, user_type)
+VALUES ('Andrei Mancijo', 'andrei.mancijo@updesk.com', '123456', 'TI', 'Supervisor');
+
+INSERT INTO Usuario (nome, email, senha, setor, user_type)
+VALUES ('Jonatas Souza', 'jonatas.souza@updesk.com', 'UpDesk@123', 'TI', 'Supervisor');
+
+INSERT INTO Usuario (nome, email, senha, setor, user_type)
+VALUES ('Mariozan Junior', 'mariozan.junior@updesk.com', '123456', 'TI', 'Supervisor');
+
+INSERT INTO Usuario (nome, email, senha, setor, user_type)
+VALUES ('Filipe Vitor', 'filipe.vitor@updesk.com', '123456', 'TI', 'Supervisor');
+
+-- SELECT * FROM Usuario;
