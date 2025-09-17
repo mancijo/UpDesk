8f383140b422c313a5fb032fbeeb5480dec05171
Andrei <andrei.mancijo1@gmail.com>
2025-09-17 19:27:47 -0300

    react att

---
+++ updesk-mobile/app/index.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, View } from "react-native";
+import Card from "../components/Card";
+
+export default function Screen () {
+    return (
+        <Card>
+            <View>Olá</View>
+        </Card>
+    )
+}
+
