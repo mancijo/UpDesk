1378a53ab895e0e06cf0054e76f508c460a77bba
Andrei <andrei.mancijo1@gmail.com>
2025-09-17 19:06:40 -0300

    config package.json for expo-router

---
+++ updesk-mobile/components/Card.tsx
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const Card = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <View style={styles.card}>
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 5, // sm rounding
+    padding: 16,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+});
+
+export default Card;
